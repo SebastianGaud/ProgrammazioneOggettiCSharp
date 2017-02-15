049bd8fa3ad809e0a863cf5ba808ec390ac05d49
Sebastiano <sebastiano.gaudeano@live.com>
2017-02-15 11:03:38 +0100

    Aggiunti alcuni esempi di inizializzazione di classe

---
+++ OOPConsoleLibreriaVeicoli/OOPConsoleLibreriaVeicoli.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OOPVeicoli;
+
+namespace OOPConsoleLibreriaVeicoli
+{
+    class OOPConsoleLibreriaVeicoli
+    {
+        static void Main ( string[] args )
+        {
+            Veicolo Trattore = new Veicolo( "Landini" , 0125458 , 1998 );
+            Auto GolfAuto = new Auto( "Golf" , 123458 , 2016, 5 );
+            Taxi Taxi45 = new Taxi( "Prius" , 454654 , 2012 , 789 );
+        }
+    }
+}
