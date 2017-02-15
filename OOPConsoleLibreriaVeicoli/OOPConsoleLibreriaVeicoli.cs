5e63af56a160f6ee4c160efef2065925365b8932
Sebastiano <sebastiano.gaudeano@live.com>
2017-02-15 11:20:33 +0100

    Testate le classi

--- OOPConsoleLibreriaVeicoli/OOPConsoleLibreriaVeicoli.cs
+++ OOPConsoleLibreriaVeicoli/OOPConsoleLibreriaVeicoli.cs
@@ -14,8 +14,13 @@ namespace OOPConsoleLibreriaVeicoli
             Veicolo Trattore = new Veicolo( "Landini" , 1998 , 123456789 );
             Auto GolfAuto = new Auto( "Golf" , 2016 , 0123456, 5 );
             Taxi Taxi45 = new Taxi( "Prius" , 2012 , 0123456789 , 789 );
+            Moto Ducati = new Moto( "Yamaha" , 2003 , 123456789 , "Sportiva" );
 
-            Console.WriteLine(Trattore.ToString());
+            Console.WriteLine($"Veicolo: {Trattore.ToString()}\n\n"+
+                $"Auto: {GolfAuto.ToString()}\n\n"+
+                $"Taxi: {Taxi45.ToString()}\n\n"+
+                $"Moto: {Ducati.ToString()}"
+                );
             
         }
     }
